911c4d96540674e7765bf734ac140b474ed18114
Ali Husseini <macbookpro@Apples-MacBook-Pro.local>
2022-09-23 22:51:39 +0300

    adding the js

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,12 +1,31 @@
 	
+const newsField = document.getElementById("news-field");
+
+const fillNews = () => {
 $.getJSON( "http://localhost/bootstrap-jquery/newsapi.php", function( data ) {
-  let items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + key + val + "</li>" );
-  });
- 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
+for (const news in data) {
+    newsField.innerHTML +='<div class="row gx-5">
+    <div class="col-md-6 mb-4">
+      <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5" data-mdb-ripple-color="light">
+        <img src="${news.image}" class="img-fluid" />
+        <a href="#!">
+          <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-6 mb-4">
+      <span class="badge bg-danger px-2 py-1 shadow-1-strong mb-3">${news.date}</span>
+      <h4><strong>${news.title}</strong></h4>
+      <p class="text-muted">
+      ${news.description}
+      </p>
+    </div>
+  </div>  ';
+
+}
+
 });
+}
+
+
+fillNews();
